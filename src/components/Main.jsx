a725649d3ff69f1adf5851c301c35f0bf5ea7b83
Charles Rooney <crooney36@gmail.com>
2023-03-20 10:59:45 -0400

    user requests and additions to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, MyRoutines, Activities, Register } from "/components";
 import { Navbar } from "../components";
 
 const Main = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, [token]);
+
   return (
     <div id="main">
       <Navbar />
---
+++ src/components/MyRoutines.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyRoutines = () => {
+
+}
+
+export default MyRoutines;
