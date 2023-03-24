f74a5bd3a112cd9bfee5fa34200b3812af72d4d6
Charles Rooney <crooney36@gmail.com>
2023-03-24 11:08:59 -0400

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import gym from "../assets/gym.jpg";
 
 const Home = (props) => {
   const user = localStorage.getItem("username");
   return (
     <div id="home">
       <div id="home-title">
-        {user ? <h1>Welcome, {user} !</h1> : <h1>Welcome!</h1>}
+        {user ? (
+          <h1>Welcome, {user} !</h1>
+        ) : (
+          <h1>Welcome, please sign in or register!</h1>
+        )}
+      </div>
+      <div id="image-container">
+        <img src={gym} alt="gym image" />
       </div>
     </div>
   );
